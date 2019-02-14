646ece6bde66609d6af08727623a6d19c7820cf4
Hendri Pretorius <pretorh@gmail.com>
2019-02-14 16:47:43 +0200

    arch components: add live data field on MainViewModel

--- app/src/main/java/com/pretorh/myapplication/MainActivity.kt
+++ app/src/main/java/com/pretorh/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pretorh.myapplication
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,5 +18,7 @@ class MainActivity : AppCompatActivity() {
         val model = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
         textView.text = model.r.toString()
+
+        model.currentName.observe(this, Observer<String> { name -> textView2.text = name })
     }
 }
--- app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
+++ app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
@@ -1,11 +1,13 @@
 package com.pretorh.myapplication
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class MainViewModel: ViewModel() {
     val r = Random.nextInt(100, 140)
+    val currentName: MutableLiveData<String> by lazy { MutableLiveData<String>() }
 
     init {
         Log.d(TAG, "created MainViewModel, r=$r")
