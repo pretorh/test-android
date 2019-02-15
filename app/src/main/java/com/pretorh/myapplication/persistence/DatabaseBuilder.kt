35fc63e843e37baad642f668b1d72923d7afc48f
Hendri Pretorius <pretorh@gmail.com>
2019-02-15 19:25:33 +0200

    arch components: add db migration from v1 to 2

--- app/src/main/java/com/pretorh/myapplication/persistence/DatabaseBuilder.kt
+++ app/src/main/java/com/pretorh/myapplication/persistence/DatabaseBuilder.kt
@@ -2,9 +2,18 @@ package com.pretorh.myapplication.persistence
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 fun buildDatabase(context: Context): MyDatabase {
+    val v2Upgrade = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `dummy` (`id` INTEGER NOT NULL, PRIMARY KEY(`id`))")
+        }
+    }
+
     return Room
         .databaseBuilder(context, MyDatabase::class.java, "database-name")
+        .addMigrations(v2Upgrade)
         .build()
 }
