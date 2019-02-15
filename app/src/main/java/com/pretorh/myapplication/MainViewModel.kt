340ea54f757b6dee746eb2f1ba532ddf6f5ad6ce
Hendri Pretorius <pretorh@gmail.com>
2019-02-15 18:49:51 +0200

    initialize a scheduler in the application, get reference in MainViewModel

--- app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
+++ app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
@@ -6,18 +6,22 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.pretorh.myapplication.persistence.UserRepository
+import java.util.concurrent.ExecutorService
 import kotlin.random.Random
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: UserRepository
+    private val executor: ExecutorService
 
     val r = Random.nextInt(100, 140)
     val currentName: MutableLiveData<String> by lazy { MutableLiveData<String>() }
     val firstName: LiveData<String>
+
     init {
         Log.d(TAG, "created MainViewModel, r=$r")
         val dao = (application as MyApplication).database.user()
         repository = UserRepository(dao)
+        executor = application.ioExecutor
         firstName = repository.getUserFirstName
     }
 
--- app/src/main/java/com/pretorh/myapplication/MyApplication.kt
+++ app/src/main/java/com/pretorh/myapplication/MyApplication.kt
@@ -3,12 +3,16 @@ package com.pretorh.myapplication
 import android.app.Application
 import com.pretorh.myapplication.persistence.MyDatabase
 import com.pretorh.myapplication.persistence.buildDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MyApplication : Application() {
     lateinit var database: MyDatabase
+    lateinit var ioExecutor: ExecutorService
 
     override fun onCreate() {
         super.onCreate()
         database = buildDatabase(this)
+        ioExecutor = Executors.newSingleThreadExecutor()
     }
 }
