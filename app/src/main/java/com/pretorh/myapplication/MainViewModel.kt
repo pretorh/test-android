8d34670188333b83154fb6af607d7d7c0a1a2e48
Hendri Pretorius <pretorh@gmail.com>
2019-02-15 17:52:20 +0200

    arch components: add user repository to MainViewModel

--- app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
+++ app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
@@ -4,14 +4,19 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.pretorh.myapplication.persistence.UserRepository
 import kotlin.random.Random
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: UserRepository
+
     val r = Random.nextInt(100, 140)
     val currentName: MutableLiveData<String> by lazy { MutableLiveData<String>() }
 
     init {
         Log.d(TAG, "created MainViewModel, r=$r")
+        val dao = (application as MyApplication).database.user()
+        repository = UserRepository(dao)
     }
 
     companion object {
