512e2055b5c4447316aa983cf917c702a96ce7ee
Hendri Pretorius <pretorh@gmail.com>
2019-02-16 17:14:55 +0200

    utils: extract BaseViewModel

--- app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
+++ app/src/main/java/com/pretorh/myapplication/MainViewModel.kt
@@ -2,14 +2,14 @@ package com.pretorh.myapplication
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.pretorh.myapplication.core.BaseViewModel
 import com.pretorh.myapplication.persistence.UserRepository
 import java.util.concurrent.ExecutorService
 import kotlin.random.Random
 
-class MainViewModel(application: Application) : AndroidViewModel(application) {
+class MainViewModel(application: Application) : BaseViewModel(application) {
     private val repository: UserRepository
     private val executor: ExecutorService
 
---
+++ app/src/main/java/com/pretorh/myapplication/core/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.pretorh.myapplication.core
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+abstract class BaseViewModel(application: Application) : AndroidViewModel(application)
