176526ed383e40ac9bb456fc5ba47dbaac992aed
Hendri Pretorius <pretorh@gmail.com>
2019-02-16 18:42:58 +0200

    pass retrofit into UserRepository

--- app/src/main/java/com/pretorh/myapplication/di/PersistenceModule.kt
+++ app/src/main/java/com/pretorh/myapplication/di/PersistenceModule.kt
@@ -6,6 +6,7 @@ import com.pretorh.myapplication.persistence.UserRepository
 import com.pretorh.myapplication.persistence.buildDatabase
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
@@ -15,5 +16,6 @@ class PersistenceModule(private val context: Context) {
     fun database() = buildDatabase(context)
 
     @Provides
-    fun userRepository(database: MyDatabase) = UserRepository(database.user())
+    fun userRepository(database: MyDatabase, retrofit: Retrofit) =
+        UserRepository(database.user(), retrofit)
 }
--- app/src/main/java/com/pretorh/myapplication/persistence/UserRepository.kt
+++ app/src/main/java/com/pretorh/myapplication/persistence/UserRepository.kt
@@ -3,8 +3,12 @@ package com.pretorh.myapplication.persistence
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import retrofit2.Retrofit
 
-class UserRepository(private val userDao: UserDao) {
+class UserRepository(
+    private val userDao: UserDao,
+    private val retrofit: Retrofit
+) {
     val getUser = userDao.getUser(1)
     val getUserFirstName: LiveData<String> = Transformations
         .map(userDao.getUser(1)) {
