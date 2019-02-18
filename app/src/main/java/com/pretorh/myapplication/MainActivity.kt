ed1b465edb9728179fe6aeb7070313ab6ddb4a9a
Hendri Pretorius <pretorh@gmail.com>
2019-02-18 18:28:39 +0200

    navigation: add nav host fragment to MainActivity

--- app/src/main/java/com/pretorh/myapplication/MainActivity.kt
+++ app/src/main/java/com/pretorh/myapplication/MainActivity.kt
@@ -7,11 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_activity)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
     }
 }
