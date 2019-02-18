92847648912b857f020cf6916c6784962ac082ac
Hendri Pretorius <pretorh@gmail.com>
2019-02-18 18:15:50 +0200

    add blank Fragment2

---
+++ app/src/main/java/com/pretorh/myapplication/Fragment2.kt
@@ -0,0 +1,17 @@
+package com.pretorh.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_fragment2, container, false)
+    }
+
+    companion object {
+        fun newInstance() = Fragment2()
+    }
+}
