8e627bd2b4dc6998b3df76fbb9356c66fc295b10
Hendri Pretorius <pretorh@gmail.com>
2019-02-18 19:05:19 +0200

    navigation: setup action bar in MainActivity with nav controller

--- app/src/main/java/com/pretorh/myapplication/MainActivity.kt
+++ app/src/main/java/com/pretorh/myapplication/MainActivity.kt
@@ -3,18 +3,36 @@ package com.pretorh.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main_activity.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_activity)
+        setupActionBar()
         setupNavigation()
     }
 
+    private fun setupActionBar() {
+        val navController = findNavController()
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController().navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
     private fun setupNavigation() {
-        findNavController(R.id.fragment3).addOnDestinationChangedListener { _, destination, _ ->
+        findNavController().addOnDestinationChangedListener { _, destination, _ ->
             textActiveNav.text = destination.label
         }
     }
+
+    private fun findNavController() = findNavController(R.id.fragment3)
 }
