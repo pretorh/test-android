33eac76737b6206b1ff8edc9fc2777a8b8b1f56a
Hendri Pretorius <pretorh@gmail.com>
2019-02-19 20:31:28 +0200

    navigation: load the navigation argument in Fragment2

--- app/src/main/java/com/pretorh/myapplication/Fragment2.kt
+++ app/src/main/java/com/pretorh/myapplication/Fragment2.kt
@@ -5,13 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_fragment2.*
 
 class Fragment2 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_fragment2, container, false)
     }
 
-    companion object {
-        fun newInstance() = Fragment2()
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        text.text = arguments?.getInt("arg2").toString()
     }
 }
