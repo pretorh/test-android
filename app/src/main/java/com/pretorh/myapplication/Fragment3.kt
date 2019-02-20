44d17015ef3a4b09859303a832ab641f52740e0b
Hendri Pretorius <pretorh@gmail.com>
2019-02-20 19:55:05 +0200

    navigation: setup navigation from main to fragment 3, 3 to 4

--- app/src/main/java/com/pretorh/myapplication/Fragment3.kt
+++ app/src/main/java/com/pretorh/myapplication/Fragment3.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_fragment3.*
 
 class Fragment3 : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,10 @@ class Fragment3 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment3, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        text3.setOnClickListener { text3.findNavController().navigate(R.id.action_fragment3_to_fragment4) }
     }
 }
