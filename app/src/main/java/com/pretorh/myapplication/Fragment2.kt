258d5ee102036feca84231bc8395e7c179fd5f02
Hendri Pretorius <pretorh@gmail.com>
2019-06-05 18:56:07 +0200

    download manager: request to download a file
    enqueue downloading a 5mb file using DownloadManager

--- app/src/main/java/com/pretorh/myapplication/Fragment2.kt
+++ app/src/main/java/com/pretorh/myapplication/Fragment2.kt
@@ -1,9 +1,12 @@
 package com.pretorh.myapplication
 
+import android.app.DownloadManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_fragment2.*
 
@@ -20,5 +23,11 @@ class Fragment2 : Fragment() {
     }
 
     private fun startDownload() {
+        val nonNullContext = context ?: return
+        val downloadManager = ContextCompat.getSystemService(nonNullContext, DownloadManager::class.java) ?: return
+
+        val uri = Uri.parse("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4")
+        val request = DownloadManager.Request(uri)
+        downloadManager.enqueue(request)
     }
 }
