7b846ff05ce69194c9e956fd67b3aeaca7bd23a5
Hendri Pretorius <pretorh@gmail.com>
2019-06-24 21:48:36 +0200

    tests: setup taking screenshot in tests

--- app/src/androidTest/java/com/pretorh/myapplication/SimpleActivityTest.kt
+++ app/src/androidTest/java/com/pretorh/myapplication/SimpleActivityTest.kt
@@ -1,11 +1,14 @@
 package com.pretorh.myapplication
 
+import android.graphics.Bitmap
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.screenshot.Screenshot
 import org.junit.Test
+import java.util.*
 
 class SimpleActivityTest {
     private val activity = ActivityTestRule(MainActivity::class.java, false, false)
@@ -14,5 +17,15 @@ class SimpleActivityTest {
     fun isLaunchable() {
         activity.launchActivity(null)
         onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun canTakeScreenshots() {
+        activity.launchActivity(null)
+
+        val capture = Screenshot.capture(activity.activity).apply {
+            name = "screenshot${Date().time}"
+            format = Bitmap.CompressFormat.PNG
+        }
     }
 }
