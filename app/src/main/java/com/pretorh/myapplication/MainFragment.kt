823940e062de9c1815870a530aeff9e8f5c1594a
Hendri Pretorius <pretorh@gmail.com>
2019-06-30 11:30:37 +0200

    events: use toast and snackbars in order to display both at the same time

--- app/src/main/java/com/pretorh/myapplication/MainFragment.kt
+++ app/src/main/java/com/pretorh/myapplication/MainFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -56,7 +57,7 @@ class MainFragment : Fragment() {
             event.getIfNotHandled()?.let { name ->
                 val message = "Event observer: $name"
                 if (checkbox_enable_snackbar.isChecked) {
-                    Snackbar.make(textViewFirstNameFromRepository, message, Snackbar.LENGTH_SHORT).show()
+                    Toast.makeText(context!!, message, Toast.LENGTH_SHORT).show()
                 }
                 Log.d(TAG, message)
             }
