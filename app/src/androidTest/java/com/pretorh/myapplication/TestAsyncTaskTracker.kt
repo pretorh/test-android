ed1ec923e357bde186e0133cc0ff307e54e5cfb8
Hendri Pretorius <pretorh@gmail.com>
2019-07-06 14:20:58 +0200

    tests: implement IdlingResource on TestAsyncTaskTracker

--- app/src/androidTest/java/com/pretorh/myapplication/TestAsyncTaskTracker.kt
+++ app/src/androidTest/java/com/pretorh/myapplication/TestAsyncTaskTracker.kt
@@ -1,10 +1,12 @@
 package com.pretorh.myapplication
 
 import android.util.Log
+import androidx.test.espresso.IdlingResource
 import com.pretorh.myapplication.core.AsyncTaskTracker
 import java.util.concurrent.atomic.AtomicInteger
 
-class TestAsyncTaskTracker : AsyncTaskTracker() {
+class TestAsyncTaskTracker : AsyncTaskTracker(), IdlingResource {
+    private var resourceCallback: IdlingResource.ResourceCallback? = null
     private val pending = AtomicInteger(0)
 
     override fun start() {
@@ -15,5 +17,16 @@ class TestAsyncTaskTracker : AsyncTaskTracker() {
     override fun completed() {
         val done = pending.decrementAndGet() == 0
         Log.d("AsyncTaskTracker", "completed, done=$done")
+        if (done) {
+            resourceCallback?.onTransitionToIdle()
+        }
+    }
+
+    override fun getName(): String = "TestAsyncTaskTracker"
+
+    override fun isIdleNow(): Boolean = pending.get() == 0
+
+    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback?) {
+        resourceCallback = callback
     }
 }
