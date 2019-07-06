32bb39ffdd88c457a416d0227aa0b9227d250e09
Hendri Pretorius <pretorh@gmail.com>
2019-07-06 13:35:30 +0200

    setup view model for fragment 3

--- app/src/main/java/com/pretorh/myapplication/Fragment3.kt
+++ app/src/main/java/com/pretorh/myapplication/Fragment3.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_fragment3.*
 
 class Fragment3 : Fragment() {
+    private lateinit var viewModel: Fragment3ViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,5 +23,6 @@ class Fragment3 : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         text3.setOnClickListener { text3.findNavController().navigate(R.id.action_fragment3_to_fragment4) }
+        viewModel = ViewModelProviders.of(this).get(Fragment3ViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/pretorh/myapplication/Fragment3ViewModel.kt
@@ -0,0 +1,6 @@
+package com.pretorh.myapplication
+
+import androidx.lifecycle.ViewModel
+
+class Fragment3ViewModel : ViewModel() {
+}
