771d414d0cc0e754f79bdf93b1145664cc9439df
Hendri Pretorius <pretorh@gmail.com>
2019-07-06 14:10:01 +0200

    tests: implement TestAsyncTaskTracker with atomic int to track progress

--- app/src/androidTest/java/com/pretorh/myapplication/TestAsyncTaskTracker.kt
+++ app/src/androidTest/java/com/pretorh/myapplication/TestAsyncTaskTracker.kt
@@ -1,5 +1,19 @@
 package com.pretorh.myapplication
 
+import android.util.Log
 import com.pretorh.myapplication.core.AsyncTaskTracker
+import java.util.concurrent.atomic.AtomicInteger
 
-class TestAsyncTaskTracker : AsyncTaskTracker()
+class TestAsyncTaskTracker : AsyncTaskTracker() {
+    private val pending = AtomicInteger(0)
+
+    override fun start() {
+        val pendingCount = pending.incrementAndGet()
+        Log.d("AsyncTaskTracker", "started, pending count = $pendingCount")
+    }
+
+    override fun completed() {
+        val done = pending.decrementAndGet() == 0
+        Log.d("AsyncTaskTracker", "completed, done=$done")
+    }
+}
