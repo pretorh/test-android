5980b158c189d59f4e470feb836030fae97caa1e
Hendri Pretorius <pretorh@gmail.com>
2019-07-06 13:48:49 +0200

    change text async from view model
    this breaks the ui test, as the test is not waiting for the change to
    occur

--- app/src/main/java/com/pretorh/myapplication/Fragment3ViewModel.kt
+++ app/src/main/java/com/pretorh/myapplication/Fragment3ViewModel.kt
@@ -3,12 +3,16 @@ package com.pretorh.myapplication
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.concurrent.Executors
 
 class Fragment3ViewModel : ViewModel() {
     private val liveData: MutableLiveData<String> = MutableLiveData()
 
     fun getText(): LiveData<String> {
-        liveData.value = "hello from Fragment3ViewModel"
+        Executors.newSingleThreadExecutor().submit {
+            Thread.sleep(250)
+            liveData.value = "hello from Fragment3ViewModel"
+        }
         return liveData
     }
 }
