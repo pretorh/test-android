9a34803fabc9f1b88eefb98a79e41045cf8cfe07
Hendri Pretorius <pretorh@gmail.com>
2019-07-06 13:44:51 +0200

    tests: add test for Fragment3

---
+++ app/src/androidTest/java/com/pretorh/myapplication/Fragment3Tests.kt
@@ -0,0 +1,31 @@
+package com.pretorh.myapplication
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import org.junit.Test
+
+class Fragment3Tests {
+    private val activity = ActivityTestRule(MainActivity::class.java, false, false)
+
+    @Test
+    fun canLaunchFragment() {
+        launchToMainFragment()
+        navigateToFragment3()
+    }
+
+    private fun launchToMainFragment() {
+        activity.launchActivity(null)
+        Espresso.onView(ViewMatchers.withId(R.id.fragment3))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    private fun navigateToFragment3() {
+        Espresso.onView(ViewMatchers.withId(R.id.button_navigate3))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.text3))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
