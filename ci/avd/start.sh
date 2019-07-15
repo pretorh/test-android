138c804dcbc1552b41a74055090ddca382970582
Hendri Pretorius <pretorh@gmail.com>
2019-07-15 21:54:03 +0200

    avd: wait a bit more after abd says the device is online
    also wait for am get-config to pass (from connectedDebugAndroidTest)
    and then wait a few more seconds

--- ci/avd/start.sh
+++ ci/avd/start.sh
@@ -12,4 +12,12 @@ echo $emulator_pid > emulator.pid
 echo "emulator started ($emulator_pid)"
 
 adb wait-for-any-device
+echo "adb says it is online"
+
+while ! adb shell am get-config 2>/dev/null ; do
+  echo "am get-config failed"
+  sleep 1
+done
+sleep 5
+
 echo "device is ready"
