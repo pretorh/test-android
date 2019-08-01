36a988dde6617017e7478f54581001b1ce9256a5
Hendri Pretorius <pretorh@gmail.com>
2019-08-01 20:55:30 +0200

    settings: add a number-only preference
    add preference item, set the text's input type to number

--- app/src/main/java/com/pretorh/myapplication/SettingsFragment.kt
+++ app/src/main/java/com/pretorh/myapplication/SettingsFragment.kt
@@ -1,10 +1,15 @@
 package com.pretorh.myapplication
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.prefs, rootKey)
+        findPreference<EditTextPreference?>("number")?.setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_NUMBER
+        }
     }
 }
