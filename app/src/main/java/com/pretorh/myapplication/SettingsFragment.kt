308882e58416ff76e98c226e137b6be9a9d26af7
Hendri Pretorius <pretorh@gmail.com>
2019-08-01 19:33:33 +0200

    settings: change SettingsFragment into PreferenceFragmentCompat
    import PreferenceScreen xml, use that in SettingsFragment

--- app/src/main/java/com/pretorh/myapplication/SettingsFragment.kt
+++ app/src/main/java/com/pretorh/myapplication/SettingsFragment.kt
@@ -1,13 +1,10 @@
 package com.pretorh.myapplication
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.prefs, rootKey)
     }
 }
